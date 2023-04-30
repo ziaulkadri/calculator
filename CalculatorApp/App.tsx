68da20b8559e5f2166cc718ecb2c0c22c9abf4a0
ziaulkadri <ziaul.kadri@innoage.in>
2023-04-30 13:14:53 +0530

    added keyboard styles

---
+++ CalculatorApp/App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Switch, Text, View } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import { useState } from 'react';
+import { myColors } from './src/styles/Colors';
+import Button from './src/components/Button';
+import MyKeyboard from './src/components/MyKeyboard';
+
+export default function App() {
+  const [theme,setTheme]=useState('light')
+  return (
+    <ThemeContext.Provider value={theme}>
+    <SafeAreaView style={theme === 'light'?styles.container:[styles.container,{backgroundColor:'#000'}]}>
+      <StatusBar style="auto" />
+      <Switch value ={theme === 'light'}
+      onChange={()=>setTheme(theme === 'light'?'dark':'light')}/>
+      {/* <Button isGray title='3' onPress={()=>alert('hello')}/> */}
+      <MyKeyboard/>
+    </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+});
